59efcb05ad326779d248beb1fa75607c361eccb0
Saroj personal mac <sarojkmb@gmail.com>
2017-08-13 10:28:51 -0400

    initial commit

--- src/main/java/com/example/controller/EmployeeController.java
+++ src/main/java/com/example/controller/EmployeeController.java
@@ -6,9 +6,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.model.Customer;
 import com.example.model.Employee;
+import com.example.repository.CustomerRepository;
 import com.example.service.EmployeeService;
 
 @RestController
@@ -16,6 +19,9 @@ public class EmployeeController {
 	
 	@Autowired
 	private EmployeeService employeeService;
+	
+	@Autowired
+	CustomerRepository repository;
 	
     @RequestMapping(value = "/employee", method = RequestMethod.GET)
     public List<Employee> getEmployees() {
@@ -26,6 +32,48 @@ public class EmployeeController {
     public Employee getEmployee(@PathVariable("id") long id) {
 		return employeeService.getEmployeeById(id);
 	}
+    
+    @RequestMapping("/save")
+	public String process(){
+		repository.save(new Customer("Jack", "Smith"));
+		repository.save(new Customer("Adam", "Johnson"));
+		repository.save(new Customer("Kim", "Smith"));
+		repository.save(new Customer("David", "Williams"));
+		repository.save(new Customer("Peter", "Davis"));
+		return "Done";
+	}
+	
+	
+	@RequestMapping("/findall")
+	public String findAll(){
+		String result = "<html>";
+		
+		for(Customer cust : repository.findAll()){
+			result += "<div>" + cust.toString() + "</div>";
+		}
+		
+		return result + "</html>";
+	}
+	
+	@RequestMapping("/findbyid")
+	public String findById(@RequestParam("id") long id){
+		String result = "";
+		result = repository.findOne(id).toString();
+		return result;
+	}
+	
+	@RequestMapping("/findbylastname")
+	public String fetchDataByLastName(@RequestParam("lastname") String lastName){
+		String result = "<html>";
+		
+		for(Customer cust: repository.findByLastName(lastName)){
+			result += "<div>" + cust.toString() + "</div>"; 
+		}
+		
+		return result + "</html>";
+	}
+    
+    
 }
 
 
